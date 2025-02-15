efd8d3acac06cb6b60656f8d1d302a8b42644f1d
karthik jeenu <karthikjeenu@gmail.com>
2025-02-15 13:42:25 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,7 +1,6 @@
 package demo;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -10,6 +9,7 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -19,29 +19,121 @@ import org.testng.asserts.SoftAssert;
 import java.time.Duration;
 import java.util.logging.Level;
 
-import demo.utils.ExcelDataProvider;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import demo.utils.ExcelDP;
 import demo.wrappers.Wrappers;
 
-public class TestCases extends ExcelDataProvider{ // Lets us read the data
+public class TestCases extends ExcelDP { // Lets us read the data
         ChromeDriver driver;
+        WebDriverWait wait;
 
-        /*
-         * TODO: Write your tests here with testng @Test annotation.
-         * Follow `testCase01` `testCase02`... format or what is provided in
-         * instructions
-         */
+        @Test(priority = 1, enabled = true)
+        public void Testcase01() {
+                System.out.println("Testcase01 started");
+                Boolean URL = driver.getCurrentUrl().contains("youtube");
+                Assert.assertTrue(URL, "Youtube link is not correctly opened");
+                WebElement about = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='About']")));
+                Wrappers.clickOnBtn(driver, about);
+                String text = driver.findElement(By.xpath("//section[@class='ytabout__content']/p")).getText();
+                System.out.println(text);
+                System.out.println("Clicked on the about button");
+                System.out.println("Testcase01 ended");
+        }
 
-        /*
-         * Do not change the provided methods unless necessary, they will help in
-         * automation and assessment
-         */
+        @Test(priority = 2, enabled = true)
+        public void Testcase02() {
+                System.out.println("Testcase02 started");
+              
+                WebElement Films = wait.until(ExpectedConditions
+                                .elementToBeClickable(By.xpath("//yt-formatted-string[text()='Movies']")));
+                Wrappers.clickOnBtn(driver, Films);
+
+                WebElement RightArrow = wait.until(
+                                ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Next']")));
+                Wrappers.clickuntilbuttonisinvisible(driver, RightArrow);
+                String Comedy = driver
+                                .findElement(By.xpath(
+                                                "//ytd-grid-movie-renderer[contains(@class,'style-scope')][16]/a/span"))
+                                .getText();
+                System.out.println("Extracted Movie Text: '" + Comedy + "'"); // Debugging Output
+
+                SoftAssert softAssert = new SoftAssert();
+                softAssert.assertEquals(Comedy, "Comedy");
+
+                String certificate = driver
+                                .findElement(By.xpath(
+                                                "//ytd-grid-movie-renderer[contains(@class,'style-scope')][16]//ytd-badge-supported-renderer/div[2]/p"))
+                                .getText();
+                softAssert.assertEquals(certificate, "A");
+                System.out.println("Extracted Movie Text: '" + certificate + "'"); // Debugging Output
+                System.out.println("Testcase02 ended");
+        }
+
+        @Test(priority = 3, enabled = true)
+        public void Testcase03() throws InterruptedException {
+                System.out.println("Testcase03 started");
+                WebElement Music = wait.until(ExpectedConditions
+                                .elementToBeClickable(By.xpath("//yt-formatted-string[text()='Music']")));
+                Wrappers.clickOnBtn(driver, Music);
+                Thread.sleep(3000);
+                WebElement showmore = driver.findElement(
+                                By.xpath("(//span[contains(text(),'Biggest Hits')]/ancestor::div[@id='dismissible']//button)[1]"));
+                Wrappers.clickOnBtn(driver, showmore);
+
+
+                String lastplaylist=driver.findElement(By.xpath("//span[text()='Bollywood Dance Hitlist']")).getText();
+                System.out.println(lastplaylist);
+                WebElement songs=driver.findElement(By.xpath("//span[text()='Bollywood Dance Hitlist']/../../../../../../a/yt-collection-thumbnail-view-model/yt-collections-stack/div/div[3]/yt-thumbnail-view-model/yt-thumbnail-overlay-badge-view-model/yt-thumbnail-badge-view-model/badge-shape/div[2]"));
+
+                String text= songs.getText().replaceAll("[^0-9]", "");
+                int number=Integer.parseInt(text);
+                SoftAssert softAssert = new SoftAssert();
+                softAssert.assertTrue(number<=50,"Number is not less or equal to 50");
+                System.out.println("Testcase03 ended");
+        }
+
+
+        @Test(priority = 4, enabled = true)
+    public void TestCase04() {
+        System.out.println("Testcase04 started");
+        By newsTab = By.xpath("//a[contains(@title, 'News')]");
+        Wrappers.scrollToViewport(driver, newsTab);
+        Wrappers.clickAW(driver, newsTab);
+
+        // Locate the "Latest News Posts" section
+        By latestNewsPosts = By.xpath("//span[contains(text(), 'Latest news post')]");
+        Wrappers.scrollToViewport(driver, latestNewsPosts);
+
+        // Retrieve and print the body and the number of likes for each of the first 3 news posts
+        int numberOfNewsPosts = 3;
+        By firstNNewsLocator = By.xpath("(//span[contains(text(), 'Latest news post')]/ancestor::ytd-rich-section-renderer//ytd-post-renderer)[position() <= "+ numberOfNewsPosts +"]");
+        Wrappers.getBodyAndViewCount(driver, firstNNewsLocator);
+        System.out.println("Testcase04 ended");
+    }
+
+
+    @Test(priority = 5, enabled = true, description = "Verify video views count", dataProvider = "searchTerms", dataProviderClass = ExcelDP.class)
+    public void TestCase05(String searchTerms) {
+        System.out.println("Testcase05 started");
+        // Search for the item
+        By searchBox = By.xpath("//div[@id='center']//div/form/input");
+        Wrappers.sendKeysAW(driver, searchBox, searchTerms);
+
+        // Scroll through the search results until the total views for the videos reach 10 crore
+        long totalCount = 10_00_00_000;
+        Wrappers.scrollTillVideoCountReaches(driver, totalCount);
+        System.out.println("Testcase05 ended");
+    }
+
+
+        @BeforeMethod
+    public void driverGet() {
+        // Navigate to https://www.youtube.com
+        driver.get("https://www.youtube.com");
+    }
+
         @BeforeTest
         public void startBrowser() {
                 System.setProperty("java.util.logging.config.file", "logging.properties");
-
-                // NOT NEEDED FOR SELENIUM MANAGER
-                // WebDriverManager.chromedriver().timeout(30).setup();
 
                 ChromeOptions options = new ChromeOptions();
                 LoggingPreferences logs = new LoggingPreferences();
@@ -49,19 +141,25 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
                 logs.enable(LogType.BROWSER, Level.ALL);
                 logs.enable(LogType.DRIVER, Level.ALL);
                 options.setCapability("goog:loggingPrefs", logs);
+
+                // Fix WebSocket issue
+                options.addArguments("--disable-gpu");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
                 options.addArguments("--remote-allow-origins=*");
 
                 System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
                 driver = new ChromeDriver(options);
+                wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
                 driver.manage().window().maximize();
         }
 
         @AfterTest
         public void endTest() {
-                driver.close();
-                driver.quit();
-
+                if (driver != null) {
+                        driver.quit();
+                }
         }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,15 +3,187 @@ package demo.wrappers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+
+    public static void clickOnBtn(WebDriver driver, WebElement button) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+    }
+
+    public static void clickuntilbuttonisinvisible(WebDriver driver, WebElement button) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            while (button.isDisplayed()) {
+                button.click();
+                wait.until(ExpectedConditions.visibilityOf(button));
+            }
+        } catch (Exception e) {
+            System.out.println("Button is no longer visible.");
+        }
+    }
+     public static void scrollToViewport(WebDriver driver, By locator) {
+        try {
+
+            // Set up a WebDriverWait to wait for the element to become visible
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+            // JavaScript executor to scroll the viewport to the element
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].scrollIntoView();", element);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void clickAW(WebDriver driver, By locator) {
+        try {
+         //   YouTubeUtils.logStatus("clickAW", "Clicking");
+
+            // Setting up WebDriverWait with a timeout of 20 seconds
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+            // Waiting for the element to be visible
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+            // Locating the web element
+            WebElement element = driver.findElement(locator);
+
+            // Clicking the element
+            element.click();
+        } catch (Exception e) {
+          // YouTubeUtils.logStatus("clickAW", "Exception while clicking\n" + e.getMessage());
+          System.out.println(e.getMessage());
+        }
+    }
+
+      public static List<WebElement> getElements(WebDriver driver, By firstNNewsLocator) {
+        List<WebElement> elements = new ArrayList<>();
+        try {
+          //  logStatus("getElements", "Getting elements");
+
+            // Find all elements matching the locator
+            elements = driver.findElements(firstNNewsLocator);
+        } catch (Exception e) {
+           // logStatus("getElements", "Exception\n\t\t\t" + e.getMessage());
+           System.out.println(e.getMessage());
+        }
+        return elements;
+    }
+
+    public static void getBodyAndViewCount(WebDriver driver, By firstNNewsLocator) {
+        int totalLikes = 0;
+        try {
+           // logStatus("getBodyAndViewCount", "Getting body and view count");
+
+            // Wait for the first news post to become visible
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(firstNNewsLocator));
+
+            // Retrieve all news posts matching the provided locator
+            List<WebElement> newsPosts = Wrappers.getElements(driver, firstNNewsLocator);
+
+            // Iterate through each news post
+            for (WebElement elementBody : newsPosts) {
+                int likes = 0;
+                try {
+                    // Retrieve the number of likes for the current news post
+                    WebElement numberOfLikes = elementBody.findElement(By.xpath(".//span[contains(@id, 'vote-count-middle')]"));
+                    likes = Integer.parseInt(numberOfLikes.getText());
+                } catch (Exception e) {
+                    // If likes count is not available or cannot be parsed, set likes to 0
+                    likes = 0;
+                }
+
+                // Retrieve the body content of the current news post
+                WebElement bodyElement = elementBody.findElement(By.xpath(".//div[contains(@id, 'body')]"));
+                totalLikes += likes;
+
+                System.out.println("Body:"+bodyElement.getText());
+                System.out.println("Likes:"+likes);
+               
+            }
+        } catch (Exception e) {
+    
+          System.out.println(e.getMessage());
+        }
+        System.out.println("Total likes: " + totalLikes);
+    }
+
+      public static void sendKeysAW(WebDriver driver, By locator, String text) {
+        try {
+           WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+            // Waiting for the element to be visible
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+            // Locating the web element
+            WebElement element = driver.findElement(locator);
+
+            // Clearing any existing text in the element
+            element.clear();
+
+            // Sending the specified text to the element
+            element.sendKeys(text);
+
+            // Sending ENTER key press after sending the text
+            element.sendKeys(Keys.ENTER);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void scrollTillVideoCountReaches(WebDriver driver, long totalCount) {
+        try {
+            
+            int index = 1;
+
+            // Continue scrolling until the total count reaches 0 or below
+            while (totalCount >= 0) {
+                // Wait for the video view element to become visible
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+                WebElement videoViews = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//ytd-video-renderer//span[contains(text(), 'views')])["+ index +"]")));
+
+                // Scroll the viewport to the video view element
+                Wrappers.scrollToViewport(driver, By.xpath("(//ytd-video-renderer//span[contains(text(), 'views')])["+ index +"]"));
+
+                // Subtract the views count of the current video from the total count
+                totalCount -= parseViewsCount(videoViews.getText());
+
+                // Increment the index for the next video
+                index++;
+                System.out.println(videoViews.getText());
+                System.out.println(parseViewsCount(videoViews.getText()));
+                System.out.println( (totalCount <= 0) ? "Count reached" : totalCount);
+      
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static long parseViewsCount(String viewsText) {
+        // Remove the " views" suffix from the views text
+        viewsText = viewsText.replace(" views", "");
+        long viewsCount;
+        if (viewsText.endsWith("K")) {
+            // Convert the abbreviated count with "K" suffix into a long integer
+            viewsCount = (long) (Double.parseDouble(viewsText.substring(0, viewsText.length() - 1)) * 1_000);
+        } else if (viewsText.endsWith("M")) {
+            // Convert the abbreviated count with "M" suffix into a long integer
+            viewsCount = (long) (Double.parseDouble(viewsText.substring(0, viewsText.length() - 1)) * 10_00_000);
+        } else {
+            // Convert the count without any suffix into a long integer
+            viewsCount = Long.parseLong(viewsText);
+        }
+        return viewsCount;
+    }
+
 }
